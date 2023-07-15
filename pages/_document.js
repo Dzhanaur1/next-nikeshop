b16dacfde0460011905379ec78fabaf0d5acf988
Dzhanaur1 <3923328@gmail.com>
2023-07-16 00:04:28 +0300

    fix scale

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      ></meta>
+      <Head></Head>
       <body class="page">
         <Main />
         <NextScript />
